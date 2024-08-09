125cd5dfed5c136c62d20b3246e542043be8ba1d
a1x40v <a1x40v@gmail.com>
2024-08-09 12:19:51 +0300

    Provide a service in bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+/*  The service always been included */
+// bootstrapApplication(AppComponent, {
+//     providers: [TasksService],
+//   }).catch((err) => console.error(err));
+
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
